5491d925ffd8a677accf7f1bb2ae8a4b27ff692f
estevam31 <estevamvinicius31@gmail.com>
2020-05-06 02:16:06 -0300

    :wrench: chore: add jest test environment for prisma

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testEnvironment: path.join(__dirname, './prisma/prisma.test.env.js'),
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '__tests__/utils/'],
   collectCoverageFrom: ['src/app/**/*.ts'],
